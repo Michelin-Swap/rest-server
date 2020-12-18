e06d9c9cae6593a30aa5665074587cc35bbf45fb
Your Name <you@example.com>
2020-12-18 15:27:06 +0800

    fixed

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,3 @@
 export const bech32prefix = 'midas';
 export const httpUrl = 'http://127.0.0.1:1317';  // 请修改成你的 rest-server 地址；
 export const chainId = '';
-export const mnemonic = 'dynamic forest govern fault woman beef culture canal deny citizen heavy silly option march junior bicycle truth tumble cheese total luxury this glare saddle'; // 请修改成你的钱包助记词
-
-
